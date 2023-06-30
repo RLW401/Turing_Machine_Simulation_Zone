841ccf0dfeebddc2f791e797b1cc3453543a77db
Robert L. Williams <robert.leslie.williams@protonmail.com>
2023-06-29 20:26:57 -0400

    added turing machine and machine instruction seed files

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,7 @@
 from .db import db
 from .user import User
+from .turing_machine import Turing_Machine
+from .machine_instruction import Machine_Instruction
+from .collaboration import Collaboration
+from .comment import Comment
 from .db import environment, SCHEMA
