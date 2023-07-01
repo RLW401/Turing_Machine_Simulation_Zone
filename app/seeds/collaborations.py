21fe95fceaf777340654815e540e2bdba9fbdde8
Robert L. Williams <robert.leslie.williams@protonmail.com>
2023-07-01 17:58:41 -0400

    fixed typo in collaboration seeder

--- app/seeds/collaborations.py
+++ app/seeds/collaborations.py
@@ -4,13 +4,13 @@ from sqlalchemy.sql import text
 
 def seed_collaborations():
     c_1_and_2 = Collaboration(
-        requesterId = 1,
-        addresseeId = 2,
+        requester_id = 1,
+        addressee_id = 2,
         accepted = True,
     )
     c_3_and_1 = Collaboration(
-        requesterId = 3,
-        addresseeId = 1,
+        requester_id = 3,
+        addressee_id = 1,
         accepted = False,
     )
     collab_seeds = [c_1_and_2, c_3_and_1]
