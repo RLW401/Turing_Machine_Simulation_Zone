a31f73e65bb9dc4c68d0ffe92ab71222ec8426cf
Robert L. Williams <robert.leslie.williams@protonmail.com>
2023-07-10 16:00:21 -0400

    nav bar stuff

--- react-app/src/components/LoginFormModal/index.js
+++ react-app/src/components/LoginFormModal/index.js
@@ -1,17 +1,24 @@
+// root/react-app/src/components/LoginFormModal/index.js
 import React, { useState } from "react";
 import { login } from "../../store/session";
 import { useDispatch } from "react-redux";
-import { useModal } from "../../context/Modal";
+import { Modal, useModal } from "../../context/Modal";
 import "./LoginForm.css";
 
 function LoginFormModal() {
   const dispatch = useDispatch();
+  const [showModal, setShowModal] = useState(false)
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState([]);
   const { closeModal } = useModal();
   const demoEmail = "demo@aa.io";
   const demoPassword = "password";
+
+  const loginButton = (<button className='log-in'
+      onClick={() =>{
+      setShowModal(true);
+    }}>Log In</button>);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -33,38 +40,47 @@ function LoginFormModal() {
     }
   };
 
+  const loginForm = (
+    <form className="login-form" onSubmit={handleSubmit}>
+    <h1>Log In</h1>
+    <ul>
+      {errors.map((error, idx) => (
+        <li key={idx}>{error}</li>
+      ))}
+    </ul>
+    <label>
+      Email
+      <input
+        type="text"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+    </label>
+    <label>
+      Password
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+      />
+    </label>
+    <button type="submit">Log In</button>
+    <button className="demo-login"
+    onClick={loginDemo}
+    >Demo User</button>
+  </form>
+  );
+
   return (
     <>
-      <form className="login-form" onSubmit={handleSubmit}>
-        <h1>Log In</h1>
-        <ul>
-          {errors.map((error, idx) => (
-            <li key={idx}>{error}</li>
-          ))}
-        </ul>
-        <label>
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        <button type="submit">Log In</button>
-        <button className="demo-login"
-        onClick={loginDemo}
-        >Demo User</button>
-      </form>
+      {loginButton}
+      {showModal && (
+      <Modal>
+        {loginForm}
+      </Modal>
+    )}
 
     </>
   );
--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,24 +1,68 @@
+// root/react-app/src/components/Navigation/index.js
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
+import LoginFormModal from '../LoginFormModal';
+import SignupFormModal from '../SignupFormModal';
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
 	const sessionUser = useSelector(state => state.session.user);
 
-	return (
-		<ul>
-			<li>
-				<NavLink exact to="/">Home</NavLink>
-			</li>
-			{isLoaded && (
-				<li>
-					<ProfileButton user={sessionUser} />
-				</li>
-			)}
-		</ul>
-	);
+	const homeLink = (
+		<NavLink exact to="/">
+			<h2 id="zone-heading">Turing Machine Simulation Zone</h2>
+		</NavLink>
+	  );
+
+	  let sessionLinks;
+	  if (sessionUser) {
+		sessionLinks = (
+		  <div className='session-links'>
+			<div className='profile-button'>
+			<ProfileButton user={sessionUser} />
+			</div>
+			<div className='start-group link top-link'>
+			  <NavLink to="/groups/new">Start a new group</NavLink>
+			</div>
+		  </div>
+		);
+	  } else {
+		sessionLinks = (
+		  <div className='session-links'>
+
+			<LoginFormModal />
+			<SignupFormModal />
+		  </div>
+		);
+	  }
+
+	  const topBar = (
+		<div className='top-bar'>
+		  {homeLink}
+		  {sessionLinks}
+		</div>
+	  );
+
+	  return (
+		<>
+		  {(isLoaded && topBar)}
+		</>
+	  );
+
+	// return (
+	// 	<ul>
+	// 		<li>
+	// 			<NavLink exact to="/">Home</NavLink>
+	// 		</li>
+	// 		{isLoaded && (
+	// 			<li>
+	// 				<ProfileButton user={sessionUser} />
+	// 			</li>
+	// 		)}
+	// 	</ul>
+	// );
 }
 
 export default Navigation;
