954cd702247070fe681c275090c90e31e90fe956
Robert L. Williams <robert.leslie.williams@protonmail.com>
2023-07-15 14:27:43 -0400

    added render tape component

--- react-app/src/components/TuringMachinePage/index.js
+++ react-app/src/components/TuringMachinePage/index.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink, useHistory, useParams } from "react-router-dom";
 import { getAuthorizedTMs } from "../../store/turingMachines";
+import renderTape from "../RenderTape/renderTape";
 import "./turingMachine.css"
 
 const TuringMachinePage = () => {
@@ -15,10 +16,18 @@ const TuringMachinePage = () => {
     const [instructions, setInstructions] = useState({});
     const [formattedInstructions, setFormattedInstructions] = useState('');
     const [currentMachine, setCurrentMachine] = useState(null);
+    const [initTape, setInitTape] = useState('');
     const [currentTape, setCurrentTape] = useState('');
     const [currentSymbol, setCurrentSymbol] = useState('');
+    const [blankSymbol, setBlankSymbol] = useState('');
     const [headPos, setHeadPos] = useState(0);
     const [activeInstruction, setActiveInstruction] = useState(null);
+    const [numSquares, setNumSquares] = useState(11); // Number of squares of tape to be displayed
+    // const [centralSquareIndex, setCentralSquareIndex] = useState(Math.floor(numSquares / 2));
+    const [renderedTape, setRenderedTape] = useState(null);
+
+
+
 
 
 
@@ -50,9 +59,14 @@ const TuringMachinePage = () => {
 
     useEffect(() => {
         if (machines.allIds && (machines.allIds.includes(machineId))) {
+
             setCurrentMachine(machines.byId[machineId]);
+            // console.log("machines.byId[machineId]: ", machines.byId[machineId])
+            // console.log("currentMachine: ", currentMachine)
         }
-    }, [machines.allIds, machines.byId, machineId]);
+    // }, [machines.allIds, machines.byId, machineId]);
+    }, [machines, machineId]);
+
 
     let machineNames = <li key={0}>no machines</li>
     if (machines.allIds) {
@@ -68,6 +82,29 @@ const TuringMachinePage = () => {
         </div>
     );
 
+
+
+      // set current tape
+      useEffect(() => {
+        if (currentMachine) {
+            currentMachine.currentTape ? setCurrentTape(currentMachine.currentTape) : setCurrentTape(currentMachine.initTape);
+            setBlankSymbol(currentMachine.blankSymbol);
+        }
+    // });
+      }, [currentMachine]);
+
+      // render tape
+      useEffect(() => {
+        if (currentMachine && currentTape && blankSymbol) {
+            setRenderedTape(renderTape({
+                numSquares,
+                headPos,
+                blankSymbol,
+                currentTape
+            }));
+        }
+      }, [currentMachine, currentTape, numSquares]);
+
     if (currentMachine) {
         machinePage = (
             <div className="machine-page">
@@ -75,6 +112,7 @@ const TuringMachinePage = () => {
                     <h2>{currentMachine.name}</h2>
                     <p>{currentMachine.notes}</p>
                 </div>
+                {renderedTape}
                 <div className="basics-and-instructions">
                     <div className="machine-basics">
                         <h3>Machine Basics</h3>
@@ -84,7 +122,7 @@ const TuringMachinePage = () => {
                         <p>States: {`{${currentMachine.states.split('|').join(', ')}}`}</p>
                         <p>Initial State: {currentMachine.initState}</p>
                         <p>Halting State: {currentMachine.haltingState}</p>
-                        <p>Current Tape: {currentMachine.currentTape}</p>
+                        <p>Current Tape: {currentTape}</p>
                     </div>
                     <div className="machine-instructions">
                     <h3>Machine Instructions</h3>
