53aba752c813ebc8c2e42cfd6fc5e3141507cc8d
Robert L. Williams <robert.leslie.williams@protonmail.com>
2023-08-14 00:22:18 -0400

    added error handling to signup form

--- react-app/src/components/SignupFormModal/index.js
+++ react-app/src/components/SignupFormModal/index.js
@@ -1,5 +1,5 @@
 // root/react-app/src/components/SignupFormModal/index.js
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { signUp } from "../../store/session";
 import { useModal } from "../../context/Modal";
@@ -14,33 +14,91 @@ function SignupFormModal() {
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
 	const [confirmPassword, setConfirmPassword] = useState("");
+	const [submissionAttempt, setSubmissionAttempt] = useState(false);
 	const [errors, setErrors] = useState([]);
+	const minNameLen = 2;
+	const maxNameLen = 31;
+	const minPasswordLen = 8;
+	const maxPasswordLen = 64;
+
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
-		if (password === confirmPassword) {
-			const data = await dispatch(signUp(username, firstName, lastName, email, password));
-			if (data) {
-				setErrors(data);
-			} else {
-				closeModal();
-				window.location.reload(true);
-			}
+		setSubmissionAttempt(true);
+		if (errors.length) return;
+
+		setSubmissionAttempt(false);
+		const data = await dispatch(signUp(username, firstName, lastName, email, password));
+		if (data) {
+			setSubmissionAttempt(true);
+			setErrors(data);
 		} else {
-			setErrors([
-				"Confirm Password field must be the same as the Password field",
-			]);
+			closeModal();
+			window.location.reload(true);
 		}
+		// if (password === confirmPassword) {
+		// 	const data = await dispatch(signUp(username, firstName, lastName, email, password));
+		// 	if (data) {
+		// 		setSubmissionAttempt(true);
+		// 		setErrors(data);
+		// 	} else {
+		// 		closeModal();
+		// 		window.location.reload(true);
+		// 	}
+		// } else {
+		// 	setErrors([
+		// 		"Confirm Password field must be the same as the Password field",
+		// 	]);
+		// }
 	};
 
+	const emailCheck = (eAddress) => {
+		eAddress = eAddress.split('@');
+		if (eAddress.length !== 2) return false;
+		const localPart = eAddress[0];
+		let domain = eAddress[1];
+		if (!localPart.length || !domain.length) return false;
+		domain = domain.split('.');
+		if (domain.length < 2) return false;
+		for (let i = 0; i < domain.length; i++) {
+			if (!domain[i].length) return false;
+		}
+		return true;
+	};
+
+	useEffect(() => {
+		const newErrors = [];
+		if (!emailCheck(email)) {
+			newErrors.push("Please enter a valid email address.");
+		}
+		if (password !== confirmPassword) {
+			newErrors.push("Confirm Password field must be the same as the Password field.");
+		}
+		if ((password.length > maxPasswordLen) || (password.length < minPasswordLen)) {
+			newErrors.push(`Password must be between ${minPasswordLen} and ${maxPasswordLen} characters long, inclusive.`);
+		}
+		const names = {
+			"First name": firstName,
+			"Last name": lastName,
+			"Username": username,
+		};
+		Object.keys(names).forEach((key) => {
+			const value = names[key];
+			if ((value.length > maxNameLen) || (value.length < minNameLen)) {
+				newErrors.push(`${key} must be between ${minNameLen} and ${maxNameLen} characters long, inclusive.`);
+			}
+		});
+		setErrors(newErrors);
+	}, [email, firstName, lastName, username, password, confirmPassword]);
+
 	return (
-		<form className="ls-form" onSubmit={handleSubmit}>
-		<h1>Sign Up</h1>
-		<ul>
+		<form className="ls-form signup" onSubmit={handleSubmit}>
+		<h2>Sign Up</h2>
+		{submissionAttempt && <ul>
 			{errors.map((error, idx) => (
 				<li className="error" key={idx}>{error}</li>
 			))}
-		</ul>
+		</ul>}
 		<label>
 			Email
 			<input
